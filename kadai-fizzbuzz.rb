aa7206de17209e9c865455ae788536e8af81e6f7
Ubuntu <fujieryou83@gmail.com>
2024-04-10 06:13:38 +0000

    修正

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,18 @@
 num_max = 100
 
-def FizzBuzz(a,b)
-  (a..b).each do |number|
+def FizzBuzz(number)
     if number % 3 == 0 && number % 5 == 0
-      puts "FizzBuzz"
+      return "FizzBuzz"
     elsif number % 5 == 0
-      puts "Buzz"
+      return "Buzz"
     elsif number % 3 == 0
-      puts "Fizz"
+      return "Fizz"
     else
-      puts number
+      return number
     end
-  end
 end
 
-FizzBuzz(1,num_max)
+
+(1..num_max).each do |num|
+    puts FizzBuzz(num)
+end
